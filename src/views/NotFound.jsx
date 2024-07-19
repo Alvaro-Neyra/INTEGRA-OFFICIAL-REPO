badac57e963637458e584834c40cca2f75ed4e75
Alvaro Alberto Neyra Salazar <alvaroprogramador.neyra.salazar@gmail.com>
2024-07-19 18:13:46 -0500

    Adding responsive design of the 404 page

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -4,24 +4,26 @@ import { Link } from "react-router-dom";
 import "../styles/NotFound.css";
 
 function NotFound() {
-    return(
+    return (
         <>
             <main>
                 <section className="notfound-container">
-                    <div className="error-container">
-                        <div>Oops!</div>
-                        <h1>404</h1>
-                        <div className="page-notfound">Página no encontrada</div>
-                    </div>
-                    <p className="text-notfound">No pudimos encontrar esa página.</p>
-                    <div className="suggest-container">
-                        <p>Tal vez puedas encontrar lo que necesitas aquí:</p>
-                        <div className="anchors">
-                            <Link to="/" className="red-btn">INICIO</Link>
-                            <Link to="/services" className="blue-btn">SERVICIOS</Link>
-                            <Link to="/contacts" className="yellow-btn">CONTACTO</Link>
+                    <div className="main-notfound">
+                        <div className="error-container">
+                            <div>Oops!</div>
+                            <h1>404</h1>
+                            <div className="page-notfound">Página no encontrada</div>
                         </div>
-                        <img src={arrowNotFound} alt="flecha" className="arrow-notfound" />
+                        <p className="text-notfound">No pudimos encontrar esa página.</p>
+                        <div className="suggest-container">
+                            <p>Tal vez puedas encontrar lo que necesitas aquí:</p>
+                            <div className="anchors">
+                                <Link to="/" className="red-btn">INICIO</Link>
+                                <Link to="/services" className="blue-btn">SERVICIOS</Link>
+                                <Link to="/contacts" className="yellow-btn">CONTACTO</Link>
+                            </div>
+                            <img src={arrowNotFound} alt="flecha" className="arrow-notfound" />
+                        </div>
                     </div>
                     <div className="design"></div>
                     <div className="design-circle"></div>
