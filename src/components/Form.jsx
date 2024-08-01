4729c2fe551170ba090a1a4c67e284b7a4256355
Alvaro Alberto Neyra Salazar <alvaroprogramador.neyra.salazar@gmail.com>
2024-07-31 22:40:22 -0500

    Mejorar responsive design

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,28 +1,117 @@
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 import "../styles/Form.css";
 
 function Form() {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm({
+        defaultValues: {
+            name: "",
+            email: "",
+            phone: "",
+            message: ""
+        }
+    });
+
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
+    const onSubmit = (data) => {
+        console.log(data);
+        reset();
+        setFormSubmitted(true);
+        setTimeout(() => setFormSubmitted(false), 5000);
+    };
+
     return (
         <section className="form-container">
             <h2><span>¿Tengo una pregunta?</span><span>Contáctenos</span></h2>
-            <form action="/contact-response" method="post">
+            {formSubmitted && (
+                <div className="form-submitted">
+                    <p>¡Gracias por contactarnos!</p>
+                    <p>En breve nos comunicaremos con usted.</p>
+                </div>
+            )}
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <fieldset>
                     <div className="input-container">
                         <label htmlFor="name"><i className="fa-solid fa-user"></i></label>
-                        <input type="text" id="name" name="name" required placeholder="Nombre Completo*" />
+                        <input 
+                            {...register("name", {
+                                required: {
+                                    value: true,
+                                    message: "Por favor, ingrese su nombre completo"
+                                },
+                                minLength: {
+                                    value: 3,
+                                    message: "El nombre debe tener al menos 3 caracteres"
+                                }
+                            })}
+                            type="text" 
+                            id="name" 
+                            name="name" 
+                            placeholder="Nombre Completo*"
+                        />
                     </div>
+                    {errors.name && <p className="error">{errors.name.message}</p>}
                     <div className="input-container">
                         <label htmlFor="email"><i className="fa-solid fa-envelope"></i></label>
-                        <input type="email" id="email" name="email" required placeholder="Correo*" />
+                        <input 
+                            {...register("email", {
+                                required: {
+                                    value: true,
+                                    message: "Por favor, ingrese su correo"
+                                },
+                                pattern: {
+                                    value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+                                    message: "Por favor, ingrese un correo válido"
+                                }
+                            })}
+                            type="email" 
+                            id="email" 
+                            name="email" 
+                            placeholder="Correo*"
+                        />
                     </div>
+                    {errors.email && <p className="error">{errors.email.message}</p>}
                     <div className="input-container">
                         <label htmlFor="phone"><i className="fa-solid fa-mobile"></i></label>
-                        <input type="tel" id="phone" name="phone" placeholder="Teléfono*" />
+                        <input 
+                            {...register("phone", {
+                                required: {
+                                    value: true,
+                                    message: "Por favor, ingrese su teléfono"
+                                },
+                                pattern: {
+                                    value: /^(\+?(\d{1,3}))?[\s.-]?(\(?\d{2,4}\)?[\s.-]?)?\d{4,8}[\s.-]?\d{4}?$/,
+                                    message: "Por favor, ingrese un teléfono válido"
+                                }
+                            })}
+                            type="tel" 
+                            id="phone"
+                            name="phone"
+                            placeholder="Teléfono* (Con código de país)"
+                        />
                     </div>
+                    {errors.phone && <p className="error">{errors.phone.message}</p>}
                     <div className="input-container">
-                        <label htmlFor="message"><i class="fa-solid fa-message"></i></label>
-                        <textarea id="message" name="message" required placeholder="Mensaje"></textarea>
+                        <label htmlFor="message"><i className="fa-solid fa-message"></i></label>
+                        <textarea 
+                            {...register("message", {
+                                required: {
+                                    value: true,
+                                    message: "Por favor, ingrese un mensaje"
+                                },
+                                minLength: {
+                                    value: 10,
+                                    message: "El mensaje debe tener al menos 10 caracteres"
+                                }
+                            })}
+                            id="message"
+                            name="message"
+                            placeholder="Mensaje"
+                        />
                     </div>
-                    <input type="submit" value="Enviar"></input>
+                    {errors.message && <p className="error">{errors.message.message}</p>}
+                    <input type="submit" value="Enviar" />
                 </fieldset>
             </form>
         </section>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,7 +29,7 @@ function NavBar({ active, position }) {
             servicesRef.current.classList.add('servicios-popup-active');
         }
     };
-    
+
     const hideServices = () => {
         if (window.innerWidth <= 1024) return;
         if (servicesRef.current) {
@@ -53,7 +53,7 @@ function NavBar({ active, position }) {
     return (
         <header className={navBar ? "navbar active" : "navbar"} style={headerStyle}>
             <Link to="/">
-                <img src={integraLogo} alt="integra-logo" className='integra-img' />
+                <img src={integraLogo} alt="integra-logo" className='integra-img-first' />
             </Link>
             <nav ref={navRef}>
                 <Link to="/">
@@ -61,33 +61,35 @@ function NavBar({ active, position }) {
                 </Link>
                 <div>
                     <ul>
-                        <li>
-                            <ButtonLink active={pathname === '/'} onClick={showNavbar} to="/" className='navbar-link'>Home</ButtonLink>
-                        </li>
-                        <li>
-                            <ButtonLink active={pathname === '/nosotros'} onClick={showNavbar} to="/nosotros" className='navbar-link'>Nosotros</ButtonLink>
-                        </li>
-                        <li className="li-services-container" onMouseOver={showServices} onMouseOut={hideServices}>
-                            <ButtonLink
-                                active={pathname === '/servicios'}
-                                onClick={showNavbar}
-                                to="/servicios"
-                                className='navbar-link navbar-accordion'
-                            >
-                                Servicios
-                            </ButtonLink>
-                            <div className="servicios-popup" ref={servicesRef}>
-                                <ButtonLink width={"100%"} active={pathname === '/servicios/construccion'} to="/servicios/construccion" onClick={showNavbar} className='navbar-link'>Construcción</ButtonLink>
-                                <ButtonLink width={"100%"} active={pathname === '/servicios/gerencia-de-proyectos'} to="/servicios/gerencia-de-proyectos" onClick={showNavbar} className='navbar-link'>Gerencia de proyectos</ButtonLink>
-                                <ButtonLink width={"100%"} active={pathname === '/servicios/inmobiliaria'} to="/servicios/inmobiliaria" onClick={showNavbar} className='navbar-link'>Inmobiliaria</ButtonLink>
-                                <ButtonLink width={"100%"} active={pathname === '/servicios/expediente-tecnico'} to="/servicios/expediente-tecnico" onClick={showNavbar} className='navbar-link'>Expediente Técnico</ButtonLink>
-                                <ButtonLink width={"100%"} active={pathname === '/servicios/inspeccion-tecnica'} to="/servicios/inspeccion-tecnica" onClick={showNavbar} className='navbar-link'>Inspección Técnica</ButtonLink>
-                                <ButtonLink width={"100%"} active={pathname === '/servicios/supervision-de-obra'} to="/servicios/supervision-de-obra" onClick={showNavbar} className='navbar-link'>Supervisión de Obra</ButtonLink>
-                            </div>
-                        </li>
-                        <li>
-                            <ButtonLink active={pathname === '/portfolio'} onClick={showNavbar} to="/portfolio" className='navbar-link'>Portfolio</ButtonLink>
-                        </li>
+                        <div>
+                            <li>
+                                <ButtonLink active={pathname === '/'} onClick={showNavbar} to="/" className='navbar-link'>Home</ButtonLink>
+                            </li>
+                            <li>
+                                <ButtonLink active={pathname === '/nosotros'} onClick={showNavbar} to="/nosotros" className='navbar-link'>Nosotros</ButtonLink>
+                            </li>
+                            <li className="li-services-container" onMouseOver={showServices} onMouseOut={hideServices}>
+                                <ButtonLink
+                                    active={pathname === '/servicios'}
+                                    onClick={showNavbar}
+                                    to="/servicios"
+                                    className='navbar-link navbar-accordion'
+                                >
+                                    Servicios
+                                </ButtonLink>
+                                <div className="servicios-popup" ref={servicesRef}>
+                                    <ButtonLink width={"100%"} active={pathname === '/servicios/construccion'} to="/servicios/construccion" onClick={showNavbar} className='navbar-link'>Construcción</ButtonLink>
+                                    <ButtonLink width={"100%"} active={pathname === '/servicios/gerencia-de-proyectos'} to="/servicios/gerencia-de-proyectos" onClick={showNavbar} className='navbar-link'>Gerencia de proyectos</ButtonLink>
+                                    <ButtonLink width={"100%"} active={pathname === '/servicios/inmobiliaria'} to="/servicios/inmobiliaria" onClick={showNavbar} className='navbar-link'>Inmobiliaria</ButtonLink>
+                                    <ButtonLink width={"100%"} active={pathname === '/servicios/expediente-tecnico'} to="/servicios/expediente-tecnico" onClick={showNavbar} className='navbar-link'>Expediente Técnico</ButtonLink>
+                                    <ButtonLink width={"100%"} active={pathname === '/servicios/inspeccion-tecnica'} to="/servicios/inspeccion-tecnica" onClick={showNavbar} className='navbar-link'>Inspección Técnica</ButtonLink>
+                                    <ButtonLink width={"100%"} active={pathname === '/servicios/supervision-de-obra'} to="/servicios/supervision-de-obra" onClick={showNavbar} className='navbar-link'>Supervisión de Obra</ButtonLink>
+                                </div>
+                            </li>
+                            <li>
+                                <ButtonLink active={pathname === '/portfolio'} onClick={showNavbar} to="/portfolio" className='navbar-link'>Portfolio</ButtonLink>
+                            </li>
+                        </div>
                         <Link to="/contacto" className='contact-btn'>Contáctenos</Link>
                     </ul>
                     <button className="nav-btn nav-close-btn" onClick={showNavbar}> <FaTimes /> </button>
